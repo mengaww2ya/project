7b43f8511e9f5dd9e973133c917f0dc3753c7869
abiyandargie <147622668+abiyandargie@users.noreply.github.com>
2023-11-26 00:34:36 +0300

    Revert "the development branch"

--- Student.java
+++
@@ -1,8 +0,0 @@
-public class Student extends Person {
-    private String classroom;
-
-    public Student(String id, String name, boolean parent_permission, String classroom) {
-        super(id, name, parent_permission);
-        this.classroom = classroom;
-    }
-}
